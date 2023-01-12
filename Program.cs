1f66946073de2924e6260d1e962b51490d851bc0
natalya_kosyakina <anckamarckowa@yandex.ru>
2023-01-13 01:15:18 +0300

    Перевели схему в код, первая попытка

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int raz = 0;
+Console.WriteLine("Далеко бежать?");
+double distance = ReadLine();
+double friend1 = 2;
+double friend2 = 3;
+double dogspeed = 4;
+int tuda = 2;
+while (distance > 1)
+{
+    if (tuda == 1)
+    {
+        double time = distance/(friend1 + dogspeed);
+        tuda = 2
+    }
+    else
+    {
+        time = distance/(friend2 + dogspeed);
+        tuda = 1
+    }
+    distance = distance - (friend1 + friend 2)*time;
+raz++
+}
+Console.WriteLine(raz);
+
+
+
+Console.WriteLine("Hello, World!");
