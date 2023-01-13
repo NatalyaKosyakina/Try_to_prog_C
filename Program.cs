1ef0b046708bef1bee02d38332fbd50b13cdef65
natalya_kosyakina <anckamarckowa@yandex.ru>
2023-01-13 09:08:04 +0300

    Где же логическая ошибка, почему цикл не циклится?

--- Program.cs
+++ Program.cs
@@ -1,27 +1,24 @@
 ﻿int raz = 0;
 Console.WriteLine("Далеко бежать?");
-double distance = ReadLine();
+double distance = Console.Read();
 double friend1 = 2;
 double friend2 = 3;
 double dogspeed = 4;
 int tuda = 2;
-while (distance > 1)
+double time = 0;
+while (distance > 10)
 {
-    if (tuda == 1)
-    {
-        double time = distance/(friend1 + dogspeed);
-        tuda = 2
-    }
-    else
+    if (tuda == 2)
     {
         time = distance/(friend2 + dogspeed);
-        tuda = 1
+        tuda = 1;
     }
-    distance = distance - (friend1 + friend 2)*time;
-raz++
+    if (tuda == 1)
+    {
+       time = distance/(friend1 + dogspeed);
+        tuda = 2;
+    }
+    distance = distance - (friend1 + friend2)*time;
+raz = raz + 1;
 }
 Console.WriteLine(raz);
-
-
-
-Console.WriteLine("Hello, World!");
